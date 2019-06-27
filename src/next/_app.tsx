0614424bba12d9757d3528f0f5971510f47fd177
unknown <jake@goldcoastmaids.com.au>
2019-06-27 13:34:26 +0700

    fix: change next/_app to get working with next.js

--- src/next/_app.tsx
+++ src/next/_app.tsx
@@ -1,33 +1,25 @@
-import React from "react";
-import App, { Container } from "next/app";
+import React, { useEffect } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/styles";
+import { NextFunctionComponent } from "next";
 import theme from "../theme/createTheme";
 import { StateProvider } from "../state/store";
 
-class AppProvider extends App {
-  componentDidMount(): void {
-    // Remove the server-side injected CSS
+const AppProvider: NextFunctionComponent = ({ children }) => {
+  useEffect((): void => {
     const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles && jssStyles.parentNode) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
-  }
-
-  render(): JSX.Element {
-    const { Component, pageProps, children } = this.props;
-    return (
-      <Container>
-        <StateProvider>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Component {...pageProps} />
-            {children}
-          </ThemeProvider>
-        </StateProvider>
-      </Container>
-    );
-  }
-}
+  });
+  return (
+    <StateProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </StateProvider>
+  );
+};
 
 export default AppProvider;
